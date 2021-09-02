8af8693018923bb6a1a66e805eefb6f2fa03f8c5
jang13579 <80467890+jang13579@users.noreply.github.com>
2021-09-03 01:18:06 +0900

    초기파일 커밋

---
+++ src/main/java/ko/co/dongyang/study/minilms/IndexController.java
@@ -0,0 +1,13 @@
+package ko.co.dongyang.study.minilms;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+
+    @GetMapping("/")
+    public String index(){
+        return "index";
+    }
+}
