9f83425a292c0bdc6552dbe049855029f0cffb88
jang13579 <80467890+jang13579@users.noreply.github.com>
2021-09-03 01:38:36 +0900

    회원가입 처리중

---
+++ src/main/java/ko/co/dongyang/study/minilms/user/UserController.java
@@ -0,0 +1,14 @@
+package ko.co.dongyang.study.minilms.user;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class UserController {
+
+    @GetMapping("user/register")
+    public String register() {
+        return "user/register";
+    }
+
+}
